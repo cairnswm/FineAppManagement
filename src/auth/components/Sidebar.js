efa3ef5e400040a998c7877dbcc80eec1bceec45
fine-agent <fine@fine.dev>
2025-03-07 13:32:27 +0000

    Updated / Created files

--- src/auth/components/Sidebar.js
+++ src/auth/components/Sidebar.js
@@ -16,15 +16,17 @@ const Sidebar = () => {
         <Nav.Link as={Link} to="/home">Home</Nav.Link>
         <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
         <Nav.Link as={Link} to="/properties">Properties</Nav.Link>
+        {isAdmin && <Nav.Link as={Link} to="/settings">Settings</Nav.Link>}
         <Nav.Link as={Link} to="/payment">Payment</Nav.Link>
         <hr />
         <Nav.Link as={Link} to="/applications">Applications</Nav.Link>
+        {activeApplication && (<>
         <hr />
         <h6 className="text-muted">{activeApplication?.name || 'Data App'}</h6>
         <Nav.Link as={Link} to="/properties" style={{ marginLeft: '1rem' }}>Properties</Nav.Link>
         <Nav.Link as={Link} to="/secrets" style={{ marginLeft: '1rem' }}>Secrets</Nav.Link>
-        <Nav.Link as={Link} to="/users" style={{ marginLeft: '1rem' }}>Users</Nav.Link>
-        {isAdmin && <Nav.Link as={Link} to="/settings">Settings</Nav.Link>}
+        <Nav.Link as={Link} to="/users" style={{ marginLeft: '1rem' }}>Users</Nav.Link>        
+        </>)}
       </Nav>
     </div>
   );
